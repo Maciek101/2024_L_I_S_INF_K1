0e6bd6fbe4da8751e9d36be9abc76c2d71769630
Maciek101 <maciek101322@gmail.com>
2026-05-02 02:28:23 +0200

    wprowadzenie bledu do testów

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Maciek"
+moje_imie = "Maciek12324214"
 msg = "Hello World!"
 
 @app.route('/')
